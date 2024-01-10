69edf701b8f5fec0b860ef8b428a66f314c3cdab
cuongviethoang <hoangvietcuong2k2@gmail.com>
2024-01-10 22:58:59 +0700

    Tìm hiểu và sử dụng thư viện Axios

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -7,6 +7,8 @@ import {
     faSpinner,
 } from '@fortawesome/free-solid-svg-icons';
 import HeadlessTippy from '@tippyjs/react/headless';
+
+import * as searchService from '~/apiServices/searchService';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
 import useDebounce from '~/hooks/useDebounce';
@@ -30,21 +32,14 @@ function Search() {
             return;
         }
 
-        setLoading(true);
+        const fetchApi = async () => {
+            setLoading(true);
+            const result = await searchService.search(debounced);
+            setSearchResult(result);
+            setLoading(false);
+        };
 
-        fetch(
-            `https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(
-                debounced,
-            )}&type=less`,
-        )
-            .then((res) => res.json())
-            .then((res) => {
-                setSearchResult(res.data);
-                setLoading(false);
-            })
-            .catch(() => {
-                setLoading(false);
-            });
+        fetchApi();
     }, [debounced]);
 
     const handleClear = () => {
